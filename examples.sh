758165a7a35cf2995b277d77c58e81c0793c4ce2
Peter Schoeffman <pschoeffman@thestar.ca>
2014-03-14 08:43:57 -0400

    Adding examples shell file

---
+++ examples.sh
@@ -0,0 +1,3 @@
+#find a line with apach2 in it under and folder that starts with rc
+#execute a command to remove that line and replace it with nothing
+#sudo find ./rc* -name \*apache2 -exec rm {} \;
