d17008e093c80591db5780130f7a1b913fae7ffe
Peter Schoeffman <schoeffman@gmail.com>
2015-06-04 13:37:58 -0400

    Clean up bin

--- cmdExamples.sh
+++ cmdExamples.sh
@@ -1,3 +1,6 @@
+echo "This files exists to give examples to common commands that I always forget"
+
+
 #find a line with apach2 in it under and folder that starts with rc
 #execute a command to remove that line and replace it with nothing
 #sudo find ./rc* -name \*apache2 -exec rm {} \;
